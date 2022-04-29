da3d780e6f4d4971ec64d4041aa72b12830483dd
sarathbaby <sarathbaby33@gmail.com>
2022-04-29 12:28:46 +0530

    first commit

---
+++ src/customer/controllers/customer.controller.ts
@@ -0,0 +1,67 @@
+import { Body, Controller, forwardRef, Get, HttpCode, HttpStatus, Inject, Param, Post } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Observable } from 'rxjs';
+import { CreateCustomerDto } from '../dto/Create-Customer.dto';
+import { CustomerService } from '../services/customer.service';
+
+@Controller('customer')
+export class CustomerController {
+  constructor(
+    @Inject(forwardRef(() => CustomerService))
+    private readonly customerService: CustomerService) {}
+
+    @Get()
+    getDefault() {
+      return this.customerService.getHello();
+    }
+
+  @ApiTags('customer')
+  //@ApiBody({ type: Customer.class })
+  @ApiOkResponse({
+    description: 'Get customer',
+  })
+  @ApiNotFoundResponse({ description: 'Customer GET request failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  create(@Body() createCustomerDto: CreateCustomerDto): Observable<any> {
+    return this.customerService.createCustomer(createCustomerDto);
+  }
+
+  @ApiTags('customer')
+  //@ApiBody({ type: Customer.class })
+  @ApiOkResponse({
+    description: 'Get all customers',
+  })
+  @ApiNotFoundResponse({ description: 'Get all Customers request failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @HttpCode(HttpStatus.OK)
+  @Get('all')
+  getAllCustomersCont(): Observable<any[]> {
+    return this.customerService.getAllCustomers();
+  }
+
+  @ApiTags('customer')
+  //@ApiBody({ type: Customer.class })
+  @ApiOkResponse({
+    description: 'Get customer by Id',
+  })
+  @ApiNotFoundResponse({ description: 'Get Customer by Id request failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  getCustById(@Param('id') custId: number) {
+    return this.customerService.findCustomerById(custId);
+  }
+}
---
+++ src/order/controllers/order.controller.ts
@@ -0,0 +1,87 @@
+import { Body, Controller, forwardRef, Get, Inject, Param, Post, Put } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { OrderInputDto } from '../dto/orderinput.dto';
+import { OrderUpdateDto } from '../dto/orderUpdate.dto';
+import { OrderService } from '../services/order.service';
+
+@Controller('order')
+export class OrderController {
+  constructor(
+    @Inject(forwardRef(() => OrderService))
+    private readonly orderService: OrderService) {}
+
+  @ApiTags('order')
+  @ApiBody({ type:  OrderInputDto})
+  @ApiOkResponse({
+    description: 'Create order',
+  })
+  @ApiNotFoundResponse({ description: 'POST create order failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @Post()
+  create(@Body() orderInputDto: OrderInputDto): Promise<any> {
+    return this.orderService.createOrder(orderInputDto);
+  }
+
+  @ApiTags('order')
+  @ApiOkResponse({
+    description: 'Get Order by Id',
+  })
+  @ApiNotFoundResponse({ description: 'Get order by Id failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @Get('id/:id')
+  fetchOrderById(@Param('id') orderId: number) {
+    return this.orderService.findOrderById(orderId);
+  }
+
+  @ApiTags('order')
+  //@ApiBody({ type: Customer.class })
+  @ApiOkResponse({
+    description: 'Get All orders',
+  })
+  @ApiNotFoundResponse({ description: 'Get All orders failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @Get('all')
+  fetchAllOrders() {
+    return this.orderService.getAllOrders();
+  }
+
+  @ApiTags('order')
+  //@ApiBody({ type: Customer.class })
+  @ApiOkResponse({
+    description: 'Get All order-items',
+  })
+  @ApiNotFoundResponse({ description: 'Get All order-items failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @Get('items')
+  fetchAllOrderItemss() {
+    return this.orderService.getAllOrderItems();
+  }
+
+  @ApiTags('order')
+  //@ApiBody({ type: Customer.class })
+  @ApiOkResponse({
+    description: 'Edit Order',
+  })
+  @ApiNotFoundResponse({ description: 'Edit Order failed' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @Put('edit/:id')
+  edit(@Param('id') orderId: number, @Body() orderUpdateDto: OrderUpdateDto) {
+    return this.orderService.editOrder(orderId, orderUpdateDto);
+  }
+}
